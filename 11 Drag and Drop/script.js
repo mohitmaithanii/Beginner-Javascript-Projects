b9ffc36c21b12ce18ae6d26897104bd1e08c0bac
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-03-07 19:22:39 +0530

    add some comments

--- 11 Drag and Drop/script.js
+++ 11 Drag and Drop/script.js
@@ -3,18 +3,23 @@ const rightBox = document.getElementById("right");
 const leftBox = document.getElementById("left");
 
 for (const list of lists) {
+	// Add a "dragstart" event listener to each list element
 	list.addEventListener("dragstart", function (e) {
 		let selected = e.target;
 
+		// Add a "dragover" event listener to the "right" box element
 		rightBox.addEventListener("dragover", function (e) {
 			e.preventDefault();
 		});
 
+		// Add a "drop" event listener to the "right" box element
 		rightBox.addEventListener("drop", function (e) {
+			// Append the "selected" element as a child of the "right" box element
 			rightBox.appendChild(selected);
 			selected = null;
 		});
 
+		// Add a "dragover" event listener to the "left" box element
 		leftBox.addEventListener("dragover", function (e) {
 			e.preventDefault();
 		});
